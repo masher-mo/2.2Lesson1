0397ce2354a50e4fc30943da632399b1140c7473
bellk <bellkiss-masher@yandex.ru>
2022-10-19 17:24:05 +0300

    Task3

---
+++ Task003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, при вводе числа должна выводить соответствующему числу день недели.
+
+
+Console.Write("Введите пожалуйста число: ");
+string userNumber = Console.ReadLine();
+int dayNumber = int.Parse(userNumber);
+if (dayNumber == 1) Console.WriteLine("Понедельник");
+if (dayNumber == 2) Console.WriteLine("Вторник");
+if (dayNumber == 3) Console.WriteLine("Среда");
+if (dayNumber == 4) Console.WriteLine("Четверг");
+if (dayNumber == 5) Console.WriteLine("Пятница");
+if (dayNumber == 6) Console.WriteLine("Суббота");
+if (dayNumber == 7) Console.WriteLine("Воскресенье");
